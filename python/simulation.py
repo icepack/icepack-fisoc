25317e33b9fe5bc8435c060b7aa3cf64e3d0d2ec
danshapero <shapero.daniel@gmail.com>
2018-09-27 10:21:57 -0700

    Putting all fields into a dictionary, can talk to fortran arrays
    For some reason there's a seg fault in PETSc when we try to access
    attributes of a firedrake Function.

--- python/simulation.py
+++ python/simulation.py
@@ -4,7 +4,8 @@ import icepack, icepack.models
 from icepack.constants import gravity as g, rho_ice as ρ_I, rho_water as ρ_W, \
     glen_flow_law as n
 
-def run_simulation():
+
+def init():
     Lx, Ly = 20e3, 20e3
     u0 = 100.0
     h0, dh = 500.0, 100.0
@@ -20,7 +21,32 @@ def run_simulation():
     mesh = firedrake.RectangleMesh(32, 32, Lx, Ly)
     x, y = firedrake.SpatialCoordinate(mesh)
 
+    Q = firedrake.FunctionSpace(mesh, 'CG', 1)
+    h = firedrake.interpolate(h0 - dh * x / Lx, Q)
+    A = firedrake.interpolate(firedrake.Constant(icepack.rate_factor(T)), Q)
+
     V = firedrake.VectorFunctionSpace(mesh, 'CG', 1)
     u = firedrake.interpolate(firedrake.as_vector((exact_u(x), 0)), V)
 
-    print(u((Lx/2, Ly/2)))
+    print("Done initializing!")
+
+    return dict(velocity=u, thickness=h, fluidity=A)
+
+
+def run(fields):
+    h, u = fields['thickness'], fields['velocity']
+    A = fields['fluidity']
+
+    opts = dict(dirichlet_ids=[1], side_wall_ids=[3, 4], tol=1e-12)
+    ice_shelf = icepack.models.IceShelf()
+    u.assign(ice_shelf.diagnostic_solve(u0=u, h=h, A=A, **opts))
+
+    print(firedrake.norm(u))
+    print("Done running!")
+
+    return fields
+
+
+def get_velocity(fields):
+    print("Accessing velocity data!")
+    return fields['velocity'].dat.data_ro
--- test/wrapper_test.f90
+++ test/wrapper_test.f90
@@ -3,19 +3,70 @@ program wrapper_test
 
     use :: forpy_mod
     use :: fisoc_icepack, only: set_argv
+    use :: iso_fortran_env, only: real64
     implicit none
 
     integer :: ierror
     type(module_py) :: simulation
+    type(object) :: fields, velocity, thickness
+    type(object) :: velocity_data_object
+    type(ndarray) :: velocity_data_ndarray
+    type(tuple) :: args
+    real(kind=real64), dimension(:,:), pointer :: velocity_data
 
     ierror = forpy_initialize()
 
     call set_argv
 
     ierror = import_py(simulation, "simulation")
-    ierror = call_py_noret(simulation, "run_simulation")
+    ierror = call_py(fields, simulation, "init")
+    if (is_none(fields)) then
+        call exit(1)
+    endif
+
+    ierror = fields%getattribute(velocity, "velocity")
+    if (is_none(velocity)) then
+        stop
+    endif
+
+    ierror = fields%getattribute(thickness, "thickness")
+    if (is_none(thickness)) then
+        stop
+    endif
+
+    ierror = tuple_create(args, 1)
+    if (ierror /= 0) then
+        stop
+    endif
+    ierror = args%setitem(0, fields)
+    if (ierror /= 0) then
+        stop
+    endif
+    ierror = call_py(fields, simulation, "run", args)
+    if (ierror /= 0) then
+        stop
+    endif
+
+    ierror = call_py(velocity_data_object, simulation, "get_velocity", args)
+    if (ierror /= 0) then
+        call err_print
+        stop
+    endif
+    ierror = cast(velocity_data_ndarray, velocity_data_object)
+    if (ierror /= 0) then
+        call err_print
+        stop
+    endif
+    ierror = velocity_data_ndarray%get_data(velocity_data, 'C')
+    if (ierror /= 0) then
+        call err_print
+        stop
+    endif
+    write(*, *) size(velocity_data, 1), size(velocity_data, 2)
+    write(*, *) velocity_data(1, 1), velocity_data(2, 1)
 
     call simulation%destroy
+    call fields%destroy
 
     call forpy_finalize
 
