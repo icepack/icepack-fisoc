f95540dd361ff93a1ec796b704c0fe5fb2591a3b
danshapero <shapero.daniel@gmail.com>
2018-09-27 14:15:57 -0700

    Moved more functionality into wrapper library

---
+++ src/fisoc_icepack.F90
@@ -0,0 +1,60 @@
+
+#define check_error(ierror) if (ierror /= 0) then; call err_print; stop; endif
+
+module fisoc_icepack
+
+    use :: forpy_mod
+    use :: iso_fortran_env, only: real64
+    implicit none
+
+contains
+
+subroutine set_argv
+    integer :: ierror
+    type(module_py) :: sys
+    type(list) :: empty_list
+
+    check_error(import_py(sys, "sys"))
+    check_error(list_create(empty_list))
+    check_error(sys%setattr("argv", empty_list))
+
+    call empty_list%destroy
+    call sys%destroy
+end subroutine
+
+
+subroutine get_simulation_module(simulation)
+    type(module_py), intent(out) :: simulation
+
+    call set_argv
+    check_error(import_py(simulation, "simulation"))
+end subroutine
+
+
+subroutine simulation_initialize(simulation, state)
+    type(module_py), intent(in) :: simulation
+    type(object), intent(out) :: state
+
+    check_error(call_py(state, simulation, "init"))
+end subroutine
+
+
+subroutine simulation_get_velocity_data(simulation, state, velocity_data)
+    real(kind=real64), dimension(:,:), pointer, intent(out) :: velocity_data
+    type(module_py), intent(in) :: simulation
+    type(object), intent(in) :: state
+
+    ! local variables
+    type(tuple) :: args
+    type(object) :: velocity_object
+    type(ndarray) :: velocity_ndarray
+
+    check_error(tuple_create(args, 1))
+    check_error(args%setitem(0, state))
+    check_error(call_py(velocity_object, simulation, "get_velocity", args))
+    check_error(cast(velocity_ndarray, velocity_object))
+    check_error(velocity_ndarray%get_data(velocity_data, 'C'))
+end subroutine
+
+
+end module
--- test/wrapper_test.f90
+++ test/wrapper_test.f90
@@ -1,72 +1,27 @@
-
 program wrapper_test
 
-    use :: forpy_mod
-    use :: fisoc_icepack, only: set_argv
+    use :: forpy_mod, only: forpy_initialize, forpy_finalize, module_py, object
+    use :: fisoc_icepack, only: get_simulation_module, simulation_initialize, &
+        simulation_get_velocity_data
     use :: iso_fortran_env, only: real64
     implicit none
 
     integer :: ierror
     type(module_py) :: simulation
-    type(object) :: fields, velocity, thickness
-    type(object) :: velocity_data_object
-    type(ndarray) :: velocity_data_ndarray
-    type(tuple) :: args
+    type(object) :: state
     real(kind=real64), dimension(:,:), pointer :: velocity_data
 
     ierror = forpy_initialize()
 
-    call set_argv
+    call get_simulation_module(simulation)
+    call simulation_initialize(simulation, state)
+    call simulation_get_velocity_data(simulation, state, velocity_data)
 
-    ierror = import_py(simulation, "simulation")
-    ierror = call_py(fields, simulation, "init")
-    if (is_none(fields)) then
-        call exit(1)
-    endif
-
-    ierror = fields%getattribute(velocity, "velocity")
-    if (is_none(velocity)) then
-        stop
-    endif
-
-    ierror = fields%getattribute(thickness, "thickness")
-    if (is_none(thickness)) then
-        stop
-    endif
-
-    ierror = tuple_create(args, 1)
-    if (ierror /= 0) then
-        stop
-    endif
-    ierror = args%setitem(0, fields)
-    if (ierror /= 0) then
-        stop
-    endif
-    ierror = call_py(fields, simulation, "run", args)
-    if (ierror /= 0) then
-        stop
-    endif
-
-    ierror = call_py(velocity_data_object, simulation, "get_velocity", args)
-    if (ierror /= 0) then
-        call err_print
-        stop
-    endif
-    ierror = cast(velocity_data_ndarray, velocity_data_object)
-    if (ierror /= 0) then
-        call err_print
-        stop
-    endif
-    ierror = velocity_data_ndarray%get_data(velocity_data, 'C')
-    if (ierror /= 0) then
-        call err_print
-        stop
-    endif
     write(*, *) size(velocity_data, 1), size(velocity_data, 2)
     write(*, *) velocity_data(1, 1), velocity_data(2, 1)
 
+    call state%destroy
     call simulation%destroy
-    call fields%destroy
 
     call forpy_finalize
 
