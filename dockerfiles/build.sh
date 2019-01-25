1f8dd37b364fe990207593d5a4e61cb0c26cbd48
danshapero <shapero.daniel@gmail.com>
2019-01-25 12:44:38 -0800

    Starting Docker image for dummy ISM + ROMS

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -4,4 +4,9 @@ if [ ! -d FISOC ]; then
     git clone https://github.com/RupertGladstone/FISOC.git
 fi
 
+if [ ! -d ROMSIceShelf ]; then
+    git clone https://github.com/bkgf/ROMSIceShelf_devel.git ROMSIceShelf
+fi
+
 docker build --tag dummy --file dummy/Dockerfile .
+docker build --tag dummy-roms --file dummy-roms/Dockerfile .
