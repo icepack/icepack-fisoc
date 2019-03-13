258351ccecc9699dac0273a4d7d209db42dc9a7e
danshapero <shapero.daniel@gmail.com>
2019-03-13 15:05:35 -0700

    Cloning development branch of FISOC

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,7 +1,7 @@
 docker build --tag esmf --file esmf/Dockerfile .
 
 if [ ! -d FISOC ]; then
-    git clone https://github.com/RupertGladstone/FISOC.git
+    git clone --branch devel https://github.com/RupertGladstone/FISOC.git
 fi
 
 if [ ! -d ROMSIceShelf ]; then
