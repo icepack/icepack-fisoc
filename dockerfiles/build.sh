c42c9f9d32a7e7c722847369ea56bef530f8bd55
danshapero <shapero.daniel@gmail.com>
2019-03-13 15:06:30 -0700

    Starting on elmer/ROMS config

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -8,6 +8,11 @@ if [ ! -d ROMSIceShelf ]; then
     git clone --branch mack_local https://github.com/bkgf/ROMSIceShelf_devel.git ROMSIceShelf
 fi
 
+if [ ! -d elmerfem ]; then
+    git clone --branch elmerice_FISOC https://github.com/ElmerCSC/elmerfem.git elmerfem
+fi
+
 docker build --tag dummy-dummy --file dummy-dummy/Dockerfile .
 docker build --tag dummy-roms --file dummy-roms/Dockerfile .
 docker build --tag fool-roms --file fool-roms/Dockerfile .
+docker build --tag elmer-roms --file elmer-roms/Dockerfile .
